2c04c82ed8ea2ff66a1b89dff5e3c542a846f236
Daniel Saxton <dsaxton@pm.me>
2022-01-18 16:30:01 -0600

    Things

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,14 @@
 module.exports = {
-  root: true,
-  parser: "@typescript-eslint/parser",
-  plugins: [
-    "@typescript-eslint",
-  ],
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-  ],
-  rules: {
-      "@typescript-eslint/no-non-null-assertion": "off",
-  }
+    root: true,
+    parser: "@typescript-eslint/parser",
+    plugins: [
+        "@typescript-eslint",
+    ],
+    extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+    ],
+    rules: {
+        "@typescript-eslint/no-non-null-assertion": "off",
+    }
 };
--- webpack.config.js
+++ webpack.config.js
@@ -4,32 +4,31 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const prod = process.env.NODE_ENV === "production";
 
 module.exports = {
-  mode: prod ? "production" : "development",
-  entry: "./src/main.tsx",
-  output: {
-    path: __dirname + "/dist/",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)$/,
-        exclude: /node_modules/,
-        resolve: {
-          extensions: [".ts", ".tsx", ".js", ".json"],
-        },
-        use: "ts-loader",
-      },
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
-      },
-    ]
-  },
-  devtool: prod ? undefined : "source-map",
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "index.html",
-    }),
-    new MiniCssExtractPlugin(),
-  ],
+    mode: prod ? "production" : "development",
+    entry: "./src/main.tsx",
+    output: {
+        path: __dirname + "/dist/",
+    },
+    module: {
+        rules: [{
+                test: /\.(ts|tsx)$/,
+                exclude: /node_modules/,
+                resolve: {
+                    extensions: [".ts", ".tsx", ".js", ".json"],
+                },
+                use: "ts-loader",
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+        ]
+    },
+    devtool: prod ? undefined : "source-map",
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "index.html",
+        }),
+        new MiniCssExtractPlugin(),
+    ],
 };
