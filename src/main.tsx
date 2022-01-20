fb0e6f334b5e0b22840d608a72a643fb677dd06d
Daniel Saxton <dsaxton@pm.me>
2022-01-20 14:36:06 -0600

    Nit

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./main.css";
 import Lines from "./components/Lines";
 import Static from "./components/Static";
-import "./main.css";
 
 function App(): JSX.Element {
     return Math.random() < 0.5 ? <Lines /> : <Static />;
