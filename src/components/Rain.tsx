bff16f48c03c3483b7289a68ad294c4e640da286
Daniel Saxton <dsaxton@pm.me>
2022-01-21 13:06:04 -0600

    Fixup

--- src/components/Rain.tsx
+++ src/components/Rain.tsx
@@ -8,11 +8,12 @@ type Coordinate = {
 function Lines(props: { width: number; height: number }): JSX.Element {
     const delayTime = 100;
     const squareCount = 50;
+    const marginFactor = 20;
     const squareSize = Math.min(props.width, props.height) / 100;
-    const ceiling = (1 * props.height) / 10;
-    const floor = (9 * props.height) / 10;
-    const leftWall = (1 * props.width) / 10;
-    const rightWall = (9 * props.width) / 10;
+    const ceiling = (1 * props.height) / marginFactor;
+    const floor = ((marginFactor - 1) * props.height) / marginFactor;
+    const leftWall = (1 * props.width) / marginFactor;
+    const rightWall = ((marginFactor - 1) * props.width) / marginFactor;
     const black = "rgb(0, 0, 0)";
     const white = "rgb(255, 255, 255)";
     const darkGrey = "rgb(50, 50, 50)";
@@ -61,8 +62,10 @@ function Lines(props: { width: number; height: number }): JSX.Element {
                     let newY = y + 2 * squareSize;
                     let newX = x;
                     if (newY > floor) {
-                        newY = newY % floor;
-                        newX = Math.floor(Math.random() * props.width);
+                        newY = ceiling + (newY % floor);
+                        newX =
+                            leftWall +
+                            Math.floor(Math.random() * (rightWall - leftWall));
                     }
                     return { x: newX, y: newY };
                 });
